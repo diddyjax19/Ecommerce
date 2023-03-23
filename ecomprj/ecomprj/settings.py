82ef6d87b3e6aa6f68e48e1e64d9ea771396580d
diddyjax19 <adetobi59@gmail.com>
2023-03-23 12:02:17 +0000

    i

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -23,10 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-%o9!c3rf02q6usr!vw^s96^t*(dsv&ezbs)_u_k7^z1oa$ik0r'
 
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nestit.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
