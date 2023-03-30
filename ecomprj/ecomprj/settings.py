98e2310d600bae8b6323872977749897cc7f33cc
diddyjax19 <adetobi59@gmail.com>
2023-03-30 08:03:26 +0000

    1

--- ecomprj/ecomprj/settings.py
+++ ecomprj/ecomprj/settings.py
@@ -27,7 +27,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = False
 
-ALLOWED_HOSTS = ['nestit.herokuapp.com','nextit12.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['nextit12.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
